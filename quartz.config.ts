e68e588c55d7c1e7467faed6bdac63395dec1adb
along <2083819731@qq.com>
2025-05-15 15:01:18 +0800

    fix: update page title in quartz configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Dragonzhoulong's Garden",
+    pageTitle: "along's Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
